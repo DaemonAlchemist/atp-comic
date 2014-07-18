721d2dc85ef0daab3a74b0015d7d7ab0a73cdc6f
Andrew Wittrock <andy@wittrock.us>
2014-07-18 04:24:23 -0500

    Add toggle for arcs to show on page nav

---
+++ public/atp-comic/js/comic.js
@@ -0,0 +1,10 @@
+$(function(){
+	$(".current-arc-links").click(function(){
+		$(".current-arc-links").hide();
+		$(".all-arc-links").show();
+	});
+	$(".all-arc-links").click(function(){
+		$(".all-arc-links").hide();
+		$(".current-arc-links").show();
+	});
+});
