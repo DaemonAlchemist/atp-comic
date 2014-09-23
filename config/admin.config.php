3b5ad9a072cc87db8acaa891a19566005d40f874
Andrew Wittrock <andy@wittrock.us>
2014-09-23 09:57:30 -0500

    Add admin editing tabs

--- config/admin.config.php
+++ config/admin.config.php
@@ -9,6 +9,11 @@ return array(
 				'category' => 'Comic',
 				'displayColumns' => array('Url'),
 				'defaultOrder' => 'page_number DESC',
+				'tabs' => array(
+					'Details' => array('title', 'page_number', 'url'),
+					'Comic' => array('image_file', 'transcript_html'),
+					'Commentary' => array('description_html'),
+				),
 			),
 			'atpcomic_arc' => array(
 				'displayName' => 'Arc',
@@ -16,6 +21,10 @@ return array(
 				'category' => 'Comic',
 				'displayColumns' => array('Url'),
 				'defaultOrder' => 'name ASC',
+				'tabs' => array(
+					'Details' => array('name', 'url'),
+					'Related Arcs' => array('parent_arc_id', 'prev_arc_id', 'next_arc_id'),
+				),
 			),
 			'atpcomic_node' => array(
 				'displayName' => 'Node',
@@ -23,6 +32,10 @@ return array(
 				'category' => 'Comic',
 				'displayColumns' => array('NextNodeId', 'PrevNodeId'),
 				'defaultOrder' => 'page_id ASC',
+				'tabs' => array(
+					'Details' => array('arc_id', 'page_id'),
+					'Navigation' => array('prev_node_id', 'next_node_id'),
+				),
 			),
 		),
 	),
