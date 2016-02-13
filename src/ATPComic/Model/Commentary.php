ac2a5442360ee322e74bf750fc08b004d9ce0ada
Andrew Wittrock <andy@wittrock.us>
2016-02-13 10:30:52 -0600

    Update display name for commentaries

--- src/ATPComic/Model/Commentary.php
+++ src/ATPComic/Model/Commentary.php
@@ -8,7 +8,7 @@ class Commentary extends \ATP\ActiveRecord
 {
 	public function displayName()
 	{
-		return $this->page->title . ' by ' . $this->user->username;
+		return "'" . $this->title . "' for '" . $this->page->title . "' by " . $this->user->username;
 	}
 }
 Commentary::init();
