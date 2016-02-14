f29d004b5abed4cb477812a292c29175e57ceb7a
Andrew Wittrock <andy@wittrock.us>
2016-02-13 22:33:48 -0600

    Make sure admin user class is loaded for commentaries

--- src/ATPComic/Model/Commentary.php
+++ src/ATPComic/Model/Commentary.php
@@ -6,6 +6,14 @@ require_once("Arc.php");
 
 class Commentary extends \ATP\ActiveRecord
 {
+    public function __construct()
+    {
+        parent::__construct();
+
+        //Necessary to make sure the admin user class is loaded
+        $user = new \ATPAdmin\Model\User();
+    }
+
 	public function displayName()
 	{
 		return "'" . $this->title . "' for '" . $this->page->title . "' by " . $this->user->username;
